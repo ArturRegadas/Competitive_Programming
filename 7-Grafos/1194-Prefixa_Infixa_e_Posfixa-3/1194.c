ee8a5b67bb47009e0c5923a32b2eb0ae06583e3a
Artur Regadas <120432657+ArturRegadas@users.noreply.github.com>
2024-09-06 17:20:27 -0300

    Update 1194.c

--- 7-Grafos/1194-Prefixa_Infixa_e_Posfixa-3/1194.c
+++ 7-Grafos/1194-Prefixa_Infixa_e_Posfixa-3/1194.c
@@ -1,56 +1,56 @@
 #include <stdio.h>
+#include <string.h>
 
-int p_i(char infixa[],  char proc, int size){
-
-    for(int i=0; i<size, i++){
-        if(infixa[i]==proc){
-            return i-1;
+int findIndex(char str[], char ch, int size) {
+    for(int i = 0; i < size; i++) {
+        if (str[i] == ch) {
+            return i;
         }
     }
     return -1;
-
-
 }
 
-int main(){
-    int repeat, size;
+void infixToPostfix(char prefix[], char infix[], int size, char result[]) {
+    // Assuming the prefix expression is valid and root is always the first element
+    char root = prefix[0];
+    int rootIndex = findIndex(infix, root, size);
+    
+    if (rootIndex == -1) {
+        printf("Error: root not found in infix\n");
+        return;
+    }
 
-    char prefixa[53],infixa[53], raiz, result[53];
+    int m = 0;
+
+    // Process left subtree
+    for (int i = 0; i < rootIndex; i++) {
+        result[m++] = infix[i];
+    }
+    
+    // Process right subtree
+    for (int i = rootIndex + 1; i < size; i++) {
+        result[m++] = infix[i];
+    }
+    
+    // Add root to result
+    result[m++] = root;
+    result[m] = '\0'; // Null-terminate the result string
+}
+
+int main() {
+    int repeat, size;
+    char prefix[53], infix[53], result[53];
 
     scanf("%d", &repeat);
 
-    int qtd, nad, qtd2, c, passed, passed2;
-
-    for(int r=0; r<repeat; r++){
+    for(int r = 0; r < repeat; r++) {
         scanf("%d", &size);
-        scanf("%s", prefixa);
-        scanf("%s", infixa);
+        scanf(" %s", prefix);
+        scanf(" %s", infix);
 
-        raiz=prefixa[0];
-
-        qtd=size;
-        nad=0;
-
-        qtd2=p_i(infixa, raiz, qtd);
-
-        c=0;
-        passed2=1;
-        passed=1;
-
-        for(int i=0; i<size; i++){
-            if(nad==1){
-                break;
-            }
-            else if()
-        }
-        
-
-
-
-
-
-
-
+        infixToPostfix(prefix, infix, size, result);
+        printf("%s\n", result);
     }
 
+    return 0;
 }
