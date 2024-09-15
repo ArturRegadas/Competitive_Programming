939648bec1e8736d56f124ea81c8f3022bd4efc2
ArturRegadas <ahlregadas@gmail.com>
2024-09-15 18:34:01 -0300

    Paradigmas - 1288 c++

---
+++ 6-Paradigmas/1288-Canhão_de_Destruição-5/1288.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+int maxi(int n1, int n2){
+    if (n1>n2){
+        return n1;
+    }
+    else{
+        return n2;
+    }
+}
+
+int maxnum(int prices[],int weights[],int repeat,int size){
+
+    int maxinum[size+1]={0};
+    int current_price, current_weight;
+
+    for (int i =0; i< repeat; i++){
+        current_price=prices[i];
+        current_weight=weights[i];
+
+        for(int w = size; w > current_weight-1; w--){
+            maxinum[w]=maxi(maxinum[w], maxinum[w-current_weight]+current_price);
+        }
+
+    }
+
+    return maxinum[size];
+}
+
+int main(){
+
+    int tests, repeat, size, castle;
+    std::cin>>tests;
+    for(int r=0; r< tests; r++){
+        std::cin>>repeat;
+        int prices[repeat];
+        int weights[repeat];
+        for(int i=0; i< repeat; i++){
+            std::cin>>prices[i]>>weights[i];
+        }
+        std::cin>>size>>castle;
+        if(maxnum(prices, weights, repeat, size)>= castle){
+            std::cout<<"Missao completada com sucesso\n";
+        }
+        else{
+            std::cout<<"Falha na missao\n";
+        }
+
+
+    }
+
+    return 0;
+}
