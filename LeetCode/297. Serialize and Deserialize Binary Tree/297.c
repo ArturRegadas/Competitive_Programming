4ddca0e96c02f196b1af7bbe3356c033a455d30c
ArturRegadas <ahlregadas@gmail.com>
2024-10-21 20:37:21 -0300

    LeetCode 297 - c99

--- LeetCode/297. Serialize and Deserialize Binary Tree/297.c
+++ LeetCode/297. Serialize and Deserialize Binary Tree/297.c
@@ -1,29 +1,53 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     struct TreeNode *left;
- *     struct TreeNode *right;
- * };
- */
-/** Encodes a tree to a single string. */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//struct TreeNode {
+//    int val;
+//    struct TreeNode* left;
+//    struct TreeNode* right;
+//};
+
 char* serialize(struct TreeNode* root) {
+    if (root == NULL) {
+        return strdup("null");
+    }
 
+    char* left = serialize(root->left);
+    char* right = serialize(root->right);
+
+    int size = snprintf(NULL, 0, "%d,%s,%s", root->val, left, right);
+    char* result = (char*)malloc(size + 1);
+
+    sprintf(result, "%d,%s,%s", root->val, left, right);
+    free(left);
+    free(right);
+
+    return result;
 }
+struct TreeNode* deserializeHelper(char** data) {
+    if (**data == '\0') return NULL;
 
-/** Decodes your encoded data to tree. */
-struct TreeNode* create(int data){
-    struct TreeNode* TemporaryList = malloc(sizeof(struct TreeNode*));
-    TemporaryList -> val =data;
+    char* token = strsep(data, ",");
+    if (strcmp(token, "null") == 0) {
+        return NULL;
+    }
 
+    struct TreeNode* node = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+    node->val = atoi(token);
+    node->left = deserializeHelper(data);
+    node->right = deserializeHelper(data);
+
+    return node;
 }
-
 
 struct TreeNode* deserialize(char* data) {
-    
-    
+    char* dataCopy = strdup(data);
+    struct TreeNode* root = deserializeHelper(&dataCopy);
+    free(dataCopy);
+    return root;
 }
 
-// Your functions will be called as such:
-// char* data = serialize(root);
-// deserialize(data);
+
+
+
